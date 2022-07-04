5e2eac597a12623288b43b815f9ab38255326abc
dennisappiahALX <appiahdennis1998@gmail.com>
2022-07-04 21:24:56 +0000

    commits

--- 0x06-python-classes/0-square.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-"""Define a class square"""
-
-
-class Square:
-    """reapting a square"""
-    pass
--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string(holder={'counter': 0}):
-    holder['counter'] += 1
-    return (', '.join(['BestSchool'] * (holder['counter'])))
